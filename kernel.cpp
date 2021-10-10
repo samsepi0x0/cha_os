a9eeef468080ee9718b57dcae688897d1a6ae996
samsepi0x0 <64140687+samsepi0x0@users.noreply.github.com>
2021-10-11 00:56:51 +0530

    Add files via upload

--- kernel.cpp
+++ kernel.cpp
@@ -9,7 +9,7 @@ typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
 extern "C" void callConstructors(){
-	for(constructor* i = &start_ctors; i != end_ctors; i++){
+	for(constructor* i = &start_ctors; i != &end_ctors; i++){
 		(*i)();
 	}
 }
